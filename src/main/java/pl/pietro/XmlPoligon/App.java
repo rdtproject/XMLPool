24b8471712aa9d4f943caf666bbb4a24b208eb07
rdt.project <rdt.project@gmail.com>
2018-02-03 22:58:21 +0100

    second commit

---
+++ src/main/java/pl/pietro/XmlPoligon/App.java
@@ -0,0 +1,13 @@
+package pl.pietro.XmlPoligon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
